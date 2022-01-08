cdf35dfd8a1e319c98b2800799a586f5aafc1452
matheusteixeira7 <matheus.teixeira7@gmail.com>
2022-01-08 14:30:12 -0300

    style: add global styles

--- src/screens/Dashboard/styles.ts
+++ src/screens/Dashboard/styles.ts
@@ -5,12 +5,15 @@ const Container = styled.View`
   background-color: #fff;
   align-items: center;
   justify-content: center;
+
+  background-color: ${({ theme }) => theme.colors.background};
 `;
 
 const Title = styled.Text`
+  font-family: ${({ theme }) => theme.fonts.bold};
   font-size: 24px;
   font-weight: bold;
-  color: purple;
+  color: ${({ theme }) => theme.colors.title};
 `;
 
 export { Container, Title };
