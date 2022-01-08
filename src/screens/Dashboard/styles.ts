f62bf1b968aadd8a984809f15d000262048bd58a
matheusteixeira7 <matheus.teixeira7@gmail.com>
2022-01-08 12:17:51 -0300

    chore: first commit

---
+++ src/screens/Dashboard/styles.ts
@@ -0,0 +1,16 @@
+import styled from 'styled-components/native';
+
+const Container = styled.View`
+  flex: 1;
+  background-color: #fff;
+  align-items: center;
+  justify-content: center;
+`;
+
+const Title = styled.Text`
+  font-size: 24px;
+  font-weight: bold;
+  color: purple;
+`;
+
+export { Container, Title };
