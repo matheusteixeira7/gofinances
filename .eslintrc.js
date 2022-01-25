d7840e74fb4aa4d6098e478298bd3cb89dc6b05e
matheusteixeira7 <matheus.teixeira7@gmail.com>
2022-01-25 16:42:33 -0300

    chore: add prettier

---
+++ .eslintrc.js
@@ -0,0 +1,51 @@
+module.exports = {
+  env: {
+    es2021: true,
+  },
+  extends: [
+    'plugin:react/recommended',
+    'standard',
+    'plugin:@typescript-eslint/recommended',
+    'prettier',
+    'plugin:prettier/recommended',
+  ],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+  },
+  plugins: [
+    'react',
+    '@typescript-eslint',
+    'eslint-plugin-import-helpers',
+    'prettier',
+  ],
+  rules: {
+    'no-use-before-define': 'off',
+    '@typescript-eslint/naming-convention': [
+      'error',
+      {
+        selector: 'interface',
+        format: ['PascalCase'],
+        custom: {
+          regex: '^I[A-Z]',
+          match: true,
+        },
+      },
+    ],
+    'import-helpers/order-imports': [
+      'warn',
+      {
+        newlinesBetween: 'always',
+        groups: ['module', '/^@shared/', ['parent', 'sibling', 'index']],
+        alphabetize: {
+          order: 'asc',
+          ignoreCase: true,
+        },
+      },
+    ],
+  },
+}
